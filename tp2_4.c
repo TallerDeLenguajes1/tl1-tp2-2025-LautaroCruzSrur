8585c09b0583e10e1545d4fac28548829e63235d
Lautaro G Cruz Srur <lautaro.cruzsrur@gmail.com>
2025-04-01 15:43:49 -0300

    definiendo funciones

---
+++ tp2_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct compu
+{
+    int velocidad;
+    int anio;
+    int cantidad_nucleos;
+    char *tipo_cpu;
+};
+void listarPCs(struct compu pcs[], int cantidad);
+void mostrarMasVieja(struct compu pcs[], int cantidad);
+void mostrarMasVeloz(struct compu pcs[], int cantidad);
+
+
+int main(){
+
+}
